1acae97a70c7159331b049abab62d952359f854c
DIEGO <diegoh_c@yahoo.com>
2016-03-27 15:36:22 -0700

    testing app without mongo db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var mongojs = require('mongojs');
-var db = mongojs('trailers', ['trailers']);
+// var mongojs = require('mongojs');
+// var db = mongojs('trailers', ['trailers']);
 var bodyParser = require('body-parser');
 // app.get('/', function(req, res){
 
@@ -12,28 +12,28 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-app.get('/trailers', function(req, res){
-  console.log("Get request received in server");
-  db.trailers.find(function(err, docs){
-    res.json(docs); 
-    // console.log(docs);
-  });
-});
+// app.get('/trailers', function(req, res){
+//   console.log("Get request received in server");
+//   db.trailers.find(function(err, docs){
+//     res.json(docs); 
+//     // console.log(docs);
+//   });
+// });
 
-app.post('/trailers', function(req, res){
-  console.log(req.body);
-  db.trailers.insert(req.body, function(err, doc){
-    res.json(doc); 
-  });
-});
+// app.post('/trailers', function(req, res){
+//   console.log(req.body);
+//   db.trailers.insert(req.body, function(err, doc){
+//     res.json(doc); 
+//   });
+// });
 
-app.delete('/trailers/:id', function(req, res) {
-  var id = req.params.id;
-  console.log("id to be deleted: " + id);
-  db.trailers.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
-    res.json(doc);
-  });
-});
+// app.delete('/trailers/:id', function(req, res) {
+//   var id = req.params.id;
+//   console.log("id to be deleted: " + id);
+//   db.trailers.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+//     res.json(doc);
+//   });
+// });
 
 
 
